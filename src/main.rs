0141e73c52b852134ae8f06783a59575e67674dd
HectorOvid <sommer-tobias@web.de>
2022-03-22 19:31:45 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+use tetra::{Context, ContextBuilder, State, TetraError};
+use tetra::graphics::{self, Color, Texture};
+use tetra::input::{self, Key};
+use tetra::math::Vec2;
+
+const WINDOW_WIDTH: f32 = 1280.0;
+const WINDOW_HEIGHT: f32 = 960.0;
+
+struct GameState {
+    blue: f32,
+    color_direction: bool,
+    // NOTE: this is only an ID, quasi :)
+    paddle_texture: Texture,
+}
+
+impl GameState {
+    fn new(ctx: &mut Context) -> tetra::Result<GameState> {
+        let paddle_texture = Texture::new(ctx, "./res/bar.png")?;
+        Ok(GameState { blue: 0f32, color_direction: true, paddle_texture })
+    }
+
+    fn update_background(&mut self) -> f32 {
+        let mut b;
+        if self.color_direction {
+            b = self.blue + 0.001;
+            if b > 1f32 {
+                self.color_direction = !self.color_direction;
+                b = 1f32
+            }
+        } else {
+            b = self.blue - 0.001;
+            if b < 0f32 {
+                self.color_direction = !self.color_direction;
+                b = 0f32
+            }
+        }
+        self.blue = b;
+        b
+    }
+}
+
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
+        graphics::clear(ctx, Color::rgb(0.2, 0.7, self.update_background()));
+
+        self.paddle_texture.draw(ctx, Vec2::new(16.0, 16.0));
+
+        Ok(())
+    }
+}
+
+
+fn main() -> tetra::Result {
+    println!("Hi Simon chill");
+
+    let ran = ContextBuilder::new("Pong", WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
+        .quit_on_escape(true)
+        .build()?
+        .run(GameState::new);
+
+    // debugging / error / or whatever
+    match ran {
+        Ok(_) => {
+            println!("Hurra");
+        }
+        Err(_) => {
+            println!("Flark frigging dragons frigged it up again");
+        }
+    }
+
+    ran
+}
