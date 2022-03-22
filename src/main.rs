f151a34c260a298192006a2fd75961205c377614
HectorOvid <sommer-tobias@web.de>
2022-03-22 23:01:09 +0100

    more pixel art, and initial sprites :)

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,16 @@ struct GameState {
     color_direction: bool,
     // NOTE: this is only an ID, quasi :)
     paddle_texture: Texture,
+    other_paddle_texture: Texture,
+    ball: Texture,
 }
 
 impl GameState {
     fn new(ctx: &mut Context) -> tetra::Result<GameState> {
-        let paddle_texture = Texture::new(ctx, "./res/bar.png")?;
-        Ok(GameState { blue: 0f32, color_direction: true, paddle_texture })
+        let paddle_texture = Texture::new(ctx, "./res/bar-mini.png")?;
+        let other_paddle_texture = Texture::new(ctx, "./res/fox-pixel.png")?;
+        let ball = Texture::new(ctx, "./res/ball-pixel.png")?;
+        Ok(GameState { blue: 0f32, color_direction: true, paddle_texture, other_paddle_texture, ball })
     }
 
     fn update_background(&mut self) -> f32 {
@@ -44,6 +48,8 @@ impl State for GameState {
         graphics::clear(ctx, Color::rgb(0.2, 0.7, self.update_background()));
 
         self.paddle_texture.draw(ctx, Vec2::new(16.0, 16.0));
+        self.other_paddle_texture.draw(ctx, Vec2::new(1000.0, 740.0));
+        self.ball.draw(ctx, Vec2::new(640.0, 400.0));
 
         Ok(())
     }
